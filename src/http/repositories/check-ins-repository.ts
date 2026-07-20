33c1d6a3c9d3de356fc8f08a5c1485a6557bbba6
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2026-07-20 15:43:05 +0200

    feat(check-in): add validate check-in use case

--- src/http/repositories/check-ins-repository.ts
+++ src/http/repositories/check-ins-repository.ts
@@ -1,7 +1,9 @@
 import { type CheckIn, type Prisma } from '@prisma/client';
 
 export interface CheckInsRepository {
+  save(checkIn: CheckIn): Promise<CheckIn>;
   countByUserId(userId: string): Promise<number>;
+  findById(checkInId: string): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
