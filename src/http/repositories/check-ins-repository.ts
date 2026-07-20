6e8c17c8486cf9248344190d0d4065335e70ee1d
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2026-07-20 11:29:10 +0200

    feat(check-in): add get check ins count user case

--- src/http/repositories/check-ins-repository.ts
+++ src/http/repositories/check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { type CheckIn, type Prisma } from '@prisma/client';
 
 export interface CheckInsRepository {
+  countByUserId(userId: string): Promise<number>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
