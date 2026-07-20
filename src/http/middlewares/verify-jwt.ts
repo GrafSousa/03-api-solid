cf13226febccc4364a703e160f0632aabc625e69
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2026-07-20 20:44:16 +0200

    feat(auth): add jwt

---
+++ src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,10 @@
+import { type FastifyReply, type FastifyRequest } from 'fastify';
+
+export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    await request.jwtVerify();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    reply.status(401).send({ message: 'Unauthorized' });
+  }
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,16 @@
 import { type FastifyInstance } from 'fastify';
 
+import { verifyJwt } from './middlewares/verify-jwt';
+
+import { profile } from './controllers/profile.controller';
 import { register } from './controllers/register.controller';
 import { authenticate } from './controllers/authenticate.controller';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/session', authenticate);
+
+  /** Authenticated */
+
+  app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
